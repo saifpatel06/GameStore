3ac51a58ac1100bfffcaac2aa270fff855879ebd
saifpatel06 <saifradiocity@gmail.com>
2025-12-30 16:30:38 +0530

    Enhance game retrieval and management endpoints with proper null checks and add DELETE functionality

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -31,8 +31,13 @@ List<GameDto> games = [
 app.MapGet("/games", () => games);
 
 // GET /games/1
-app.MapGet("/games/{id}", (int id) => games.Find(game => game.Id == id))
-   .WithName(GetGameEndpointName);
+app.MapGet("/games/{id}", (int id) =>
+{
+    var game = games.Find(game => game.Id == id);
+
+    return game is null ? Results.NotFound() : Results.Ok(game);
+})
+.WithName(GetGameEndpointName);
 
 // POST /games
 app.MapPost("/games", (CreateGameDto newGame) =>
@@ -55,6 +60,11 @@ app.MapPut("/games/{id}", (int id, UpdateGameDto updatedGame) =>
 {
     var index = games.FindIndex(game => game.Id == id);
 
+    if(index == -1)
+    {
+        return Results.NotFound();
+    }
+
     games[index] = new GameDto(
         id,
         updatedGame.Name,
@@ -65,5 +75,14 @@ app.MapPut("/games/{id}", (int id, UpdateGameDto updatedGame) =>
 
     return Results.NoContent();
 });
+
+// DELETE /games/1
+app.MapDelete("/games/{id}", (int id) =>
+{
+    games.RemoveAll(game => game.Id == id);
+
+    return Results.NoContent();
+}
+);
 
 app.Run();
