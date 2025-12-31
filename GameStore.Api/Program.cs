1fb5bd73cecabbaae6683bd67ed8914ca0be7955
saifpatel06 <saifradiocity@gmail.com>
2025-12-31 11:10:34 +0530

    Database migrations

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,3 +1,4 @@
+using GameStore.Api.Data;
 using GameStore.Api.Dtos;
 using GameStore.Api.Endpoints;
 using GameStore.Api.Extensions;
@@ -7,8 +8,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddValidation();
 
+var connString = "Data Source=GameStore.db";
+builder.Services.AddSqlite<GameStoreContext>(connString);
+
 var app = builder.Build();
 
 app.MapGamesEndpoints();
+
+app.MigrateDb();
 
 app.Run();
